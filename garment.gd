14f78d3a9666847d29a10a1a78c78d44f6caa113
cis-ash <nice.tushkan@gmail.com>
2025-03-09 21:21:38 -0400

    rest of the assets, kitty variants, background

--- garment.gd
+++ garment.gd
@@ -62,21 +62,26 @@ func reset_move_reaction():
 	move_reaction = Vector2.ZERO
 	move_reaction_speed = Vector2.ZERO
 
+func pick_up():
+	z_index = 15
+	on_kitty = false
+	Cursor.instance.held_garment = self
+	grab_sound.pitch_scale = randf_range(0.85, 1.15)
+	grab_sound.play()
+	pass
+
 func _on_touch_screen_button_pressed() -> void:
 	var click_inside_closet = Closet.inside_rect.has_point(get_global_mouse_position())
 	#print("garment touched! " + name)
 	if (click_inside_closet or on_kitty) and Cursor.can_grab:
-		on_kitty = false
-		Cursor.instance.held_garment = self
-		grab_sound.pitch_scale = randf_range(0.85, 1.15)
-		grab_sound.play()
+		pick_up()
 	pass # Replace with function body.
 
 func take_off():
-	return_to_closet()
+	_return_to_closet()
 	pass
 
-func return_to_closet():
+func _return_to_closet():
 	on_kitty = false
 	if is_instance_valid(_personal_coathanger):
 		place_on_hanger()
@@ -85,10 +90,11 @@ func return_to_closet():
 	pass
 
 func drop():
-	return_to_closet()
+	_return_to_closet()
 	pass
 
 func place_on_hanger():
+	z_index = 0
 	if (is_instance_valid(_personal_coathanger)):
 		reparent(_personal_coathanger)
 		position = Vector2.ZERO
@@ -100,6 +106,7 @@ func place_on_hanger():
 	pass
 
 func prep_to_be_put_on():
+	z_index = 0
 	on_kitty = true
 	for child in get_children():
 		if not child is Node2D: continue
